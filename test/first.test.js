b99d05be771d1340e63a7c4042a932f0ce78a94a
rene_mbp <harryloveslearning@googlemail.com>
2016-09-05 17:28:47 +0200

    force failed testy

--- test/first.test.js
+++ test/first.test.js
@@ -6,4 +6,9 @@ describe('first test', function() {
 	it ('should start testing', function() {
 		assert.equal(3, 1+2);
 	});
+
+	it ('should multiply 2*4', function() {
+		assert.equal(7, 2*4);
+	});
+
 })
